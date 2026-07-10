cc08c84473edd6b3337f285717d6a91a21e97cd9
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-07-10 14:00:43 +0200

    feat: Add init_nims function.

---
+++ nim_game.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def init_nims(nims_number):
+    return [True in range(nims_number)]
+
+
+if __name__ == "__init__":
+    NIMS_NUMBER = 21
+    init_nims(NIMS_NUMBER)
